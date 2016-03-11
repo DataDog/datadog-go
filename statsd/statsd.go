db9a7306697d0ff7c079733e2c82d52459087812
sschepens <sebastian.schepens@mercadolibre.com>
2016-03-11 20:42:38 -0300

    make MaxPayloadSize a constant

--- statsd/statsd.go
+++ statsd/statsd.go
@@ -46,7 +46,7 @@ datagrams getting fragmented. The theoretical limit is 65467
 (65535 bytes Max UDP datagram size - 8byte UDP header - 60byte max IP headers)
 any number greater than that will see data loss.
 */
-var MaxPayloadSize = 1432
+const MaxPayloadSize = 1432
 
 // A Client is a handle for sending udp messages to dogstatsd.  It is safe to
 // use one Client from multiple goroutines simultaneously.
