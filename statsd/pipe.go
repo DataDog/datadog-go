b2a2d623d64e932ba7d980c11391e26cbecbde88
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2021-02-10 11:27:36 +0200

    Update statsd/pipe.go
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

--- statsd/pipe.go
+++ statsd/pipe.go
@@ -5,5 +5,5 @@ package statsd
 import "errors"
 
 func newWindowsPipeWriter(pipepath string) (statsdWriter, error) {
-	return nil, errors.New("Windows Named Pipes are not supported on other operating systems than Windows")
+	return nil, errors.New("Windows Named Pipes are only supported on Windows")
 }
