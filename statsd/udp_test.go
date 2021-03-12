1fb52eaeaabd1e39c8c87de2deee782a271c4826
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2021-03-12 16:13:07 +0000

    Fix tests

--- statsd/udp_test.go
+++ statsd/udp_test.go
@@ -21,35 +21,38 @@ func TestAddressFromEnvironment(t *testing.T) {
 	}
 
 	for _, tc := range []struct {
-		addrParam    string
-		hostEnv      string
-		portEnv      string
-		expectedAddr string
-		expectedErr  error
+		addrParam          string
+		hostEnv            string
+		portEnv            string
+		expectedWriterType string
+		expectedAddr       string
+		expectedErr        error
 	}{
 		// Nominal case
-		{"127.0.0.1:8125", "", "", "127.0.0.1:8125", nil},
+		{"127.0.0.1:8125", "", "", WriterNameUDP, "127.0.0.1:8125", nil},
 		// Parameter overrides environment
-		{"127.0.0.1:8125", "10.12.16.9", "1234", "127.0.0.1:8125", nil},
+		{"127.0.0.1:8125", "10.12.16.9", "1234", WriterNameUDP, "127.0.0.1:8125", nil},
 		// Host and port passed as env
-		{"", "10.12.16.9", "1234", "10.12.16.9:1234", nil},
+		{"", "10.12.16.9", "1234", WriterNameUDP, "10.12.16.9:1234", nil},
 		// Host passed, default port
-		{"", "10.12.16.9", "", "10.12.16.9:8125", nil},
+		{"", "10.12.16.9", "", WriterNameUDP, "10.12.16.9:8125", nil},
+		// unix socket
+		{"", "unix:///var/run/socket.sock", "", WriterNameUDS, "/var/run/socket.sock", nil},
 		// No autodetection failed
-		{"", "", "", "", errors.New("No address passed and autodetection from environment failed")},
+		{"", "", "", WriterNameUDP, "", errors.New("No address passed and autodetection from environment failed")},
 	} {
 		os.Setenv(autoHostEnvName, tc.hostEnv)
 		os.Setenv(autoPortEnvName, tc.portEnv)
 
 		// Test the error
-		writer, err := newUDPWriter(tc.addrParam)
+		writer, writerType, err := resolveAddr(tc.addrParam)
 		if tc.expectedErr == nil {
 			if err != nil {
-				t.Errorf("Unexepected error while getting writer: %s", err)
+				t.Errorf("Unexpected error while getting writer: %s", err)
 			}
 		} else {
 			if err == nil || tc.expectedErr.Error() != err.Error() {
-				t.Errorf("Unexepected error %q, got %q", tc.expectedErr, err)
+				t.Errorf("Unexpected error %q, got %q", tc.expectedErr, err)
 			}
 		}
 
@@ -62,8 +65,21 @@ func TestAddressFromEnvironment(t *testing.T) {
 			continue
 		}
 
-		if writer.remoteAddr().String() != tc.expectedAddr {
-			t.Errorf("Expected %q, got %q", tc.expectedAddr, writer.remoteAddr().String())
+		if writerType != tc.expectedWriterType {
+			t.Errorf("expected writer type %q, got %q", tc.expectedWriterType, writerType)
+		}
+
+		switch writerType {
+		case WriterNameUDP:
+			writer := writer.(*udpWriter)
+			if writer.remoteAddr().String() != tc.expectedAddr {
+				t.Errorf("Expected %q, got %q", tc.expectedAddr, writer.remoteAddr().String())
+			}
+		case WriterNameUDS:
+			writer := writer.(*udsWriter)
+			if writer.addr.String() != tc.expectedAddr {
+				t.Errorf("Expected %q, got %q", tc.expectedAddr, writer.addr.String())
+			}
 		}
 		writer.Close()
 	}
