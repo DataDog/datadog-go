276ec17fba6e4378c64d9ea1b46b26bb08d272fb
Maxime mouial <maxime.mouial@gmail.com>
2021-08-03 15:52:23 +0200

    Remove 'statsdWriter' in favor of 'io.WriteCloser'

--- statsd/uds_windows.go
+++ statsd/uds_windows.go
@@ -5,6 +5,6 @@ package statsd
 import "fmt"
 
 // newUDSWriter is disable on windows as unix sockets are not available
-func newUDSWriter(addr string) (statsdWriter, error) {
+func newUDSWriter(addr string) (io.WriteCloser, error) {
 	return nil, fmt.Errorf("unix socket is not available on windows")
 }
