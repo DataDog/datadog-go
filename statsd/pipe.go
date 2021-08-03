0f63aa1da2a8c96d57eaf139f53fa130d1e08f2e
Maxime mouial <maxime.mouial@gmail.com>
2021-08-03 15:52:23 +0200

    Remove 'statsdWriter' in favor of 'io.WriteCloser'

--- statsd/pipe.go
+++ statsd/pipe.go
@@ -4,9 +4,10 @@ package statsd
 
 import (
 	"errors"
+	"io"
 	"time"
 )
 
-func newWindowsPipeWriter(pipepath string, writeTimeout time.Duration) (statsdWriter, error) {
+func newWindowsPipeWriter(pipepath string, writeTimeout time.Duration) (io.WriteCloser, error) {
 	return nil, errors.New("Windows Named Pipes are only supported on Windows")
 }
