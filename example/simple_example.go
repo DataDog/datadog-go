6461af2ef5675f52430fc2bc5bec9114f2e715de
Maxime mouial <maxime.mouial@gmail.com>
2021-10-18 13:02:35 +0200

    Revert example/simple_example.go

--- example/simple_example.go
+++ example/simple_example.go
@@ -7,15 +7,13 @@ import (
 )
 
 func main() {
-	client, err := statsd.New("unix:///tmp/test.socket",
+	client, err := statsd.New("127.0.0.1:8125",
 		statsd.WithTags([]string{"env:prod", "service:myservice"}),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for {
-		client.Histogram("my.metrics", 21, []string{"tag1", "tag2:value"}, 1)
-	}
+	client.Histogram("my.metrics", 21, []string{"tag1", "tag2:value"}, 1)
 	client.Close()
 }
