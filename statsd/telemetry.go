a401e17917e483a12c14c74a79dc08f7871a1e27
Maxime mouial <maxime.mouial@gmail.com>
2021-10-27 14:50:35 +0200

    Releasing 4.8.3

--- statsd/telemetry.go
+++ statsd/telemetry.go
@@ -19,7 +19,7 @@ var clientTelemetryTag = "client:go"
 /*
 clientVersionTelemetryTag is a tag identifying this specific client version.
 */
-var clientVersionTelemetryTag = "client_version:4.8.2"
+var clientVersionTelemetryTag = "client_version:4.8.3"
 
 type telemetryClient struct {
 	c          *Client
