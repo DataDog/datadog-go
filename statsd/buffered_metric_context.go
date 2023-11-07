DataDog/datadog-go

commit 062e18cef60a416fb19f9203534c936198e79595

Add a way to limit the number of samples we keep for buffered metrics
Sampling rates are an inefficient mechanism to sample distributions because
it requires the user to dynamically compute the sampling rate in order to effictively
limit the load induced by distributions.

This adds `WithMaxSamplesPerContext(int)` which will limit the number of samples
we keep per contexts to a fixed number that will be high enough to stay statisically
relevant.

The sampling is done using using an algorithm called Vitter’s R), which randomly selects values
with linearly-decreasing probability. This is a commonly used algorithm in instrumentation
libraries (such as codahale). (see http://www.cs.umd.edu/~samir/498/vitter.pdf)

Additionally this fixes the computation of the `rate` for buffered metrics, this is important
because it is forwarded to the agent and passed down to the sketches in order to make sure that
we can still compute the count of events.

statsd/buffered_metric_context.go
@@ -25,10 +25,12 @@ type bufferedMetricContexts struct {
 	randomLock sync.Mutex
 }
 
-func newBufferedContexts(newMetric func(string, float64, string) *bufferedMetric) bufferedMetricContexts {
+func newBufferedContexts(newMetric func(string, float64, string, int64) *bufferedMetric, maxSamples int64) bufferedMetricContexts {
 	return bufferedMetricContexts{
-		values:    bufferedMetricMap{},
-		newMetric: newMetric,
+		values: bufferedMetricMap{},
+		newMetric: func(name string, value float64, stringTags string) *bufferedMetric {
+			return newMetric(name, value, stringTags, maxSamples)
+		},
 		// Note that calling "time.Now().UnixNano()" repeatedly quickly may return
 		// very similar values. That's fine for seeding the worker-specific random
 		// source because we just need an evenly distributed stream of float values.
@@ -51,29 +53,37 @@ func (bc *bufferedMetricContexts) flush(metrics []metric) []metric {
 }
 
 func (bc *bufferedMetricContexts) sample(name string, value float64, tags []string, rate float64) error {
-	if !shouldSample(rate, bc.random, &bc.randomLock) {
-		return nil
-	}
+	keepingSample := shouldSample(rate, bc.random, &bc.randomLock)
 
 	context, stringTags := getContextAndTags(name, tags)
+	var v *bufferedMetric = nil
 
 	bc.mutex.RLock()
-	if v, found := bc.values[context]; found {
-		v.sample(value)
-		bc.mutex.RUnlock()
-		return nil
-	}
+	v, _ = bc.values[context]
 	bc.mutex.RUnlock()
 
+	// Create it if it wasn't found
 	bc.mutex.Lock()
-	// Check if another goroutines hasn't created the value betwen the 'RUnlock' and 'Lock'
-	if v, found := bc.values[context]; found {
-		v.sample(value)
-		bc.mutex.Unlock()
-		return nil
+	if v == nil {
+		// It might have been created by another goroutine since last call
+		v, _ = bc.values[context]
+		if v == nil {
+			// If we might keep a sample that we should have skipped, but that should not drastically affect performances.
+			bc.values[context] = bc.newMetric(name, value, stringTags)
+			// We added a new value, we need to unlock the mutex and quit
+			bc.mutex.Unlock()
+			return nil
+		}
 	}
-	bc.values[context] = bc.newMetric(name, value, stringTags)
 	bc.mutex.Unlock()
+
+	// Now we can keep the sample or skip it
+	if keepingSample {
+		v.maybeKeepSample(value, bc.random, &bc.randomLock)
+	} else {
+		v.skipSample()
+	}
+
 	return nil
 }
 

statsd/metrics.go
@@ -2,6 +2,7 @@ package statsd
 
 import (
 	"math"
+	"math/rand"
 	"sync"
 	"sync/atomic"
 )
@@ -123,18 +124,59 @@ func (s *setMetric) flushUnsafe() []metric {
 type bufferedMetric struct {
 	sync.Mutex
 
+	// Kept samples (after sampling)
 	data []float64
+	// Total stored samples (after sampling)
+	storedSamples int64
+	// Total number of observed samples (before sampling). This is used to keep
+	// the sampling rate correct.
+	totalSamples int64
+
 	name string
 	// Histograms and Distributions store tags as one string since we need
 	// to compute its size multiple time when serializing.
 	tags  string
 	mtype metricType
+
+	// maxSamples is the maximum number of samples we keep in memory
+	maxSamples int64
 }
 
 func (s *bufferedMetric) sample(v float64) {
 	s.Lock()
 	defer s.Unlock()
+	s.sampleUnsafe(v)
+}
+
+func (s *bufferedMetric) sampleUnsafe(v float64) {
 	s.data = append(s.data, v)
+	s.storedSamples++
+	atomic.AddInt64(&s.totalSamples, 1)
+}
+
+func (s *bufferedMetric) maybeKeepSample(v float64, r *rand.Rand, lock *sync.Mutex) {
+	s.Lock()
+	defer s.Unlock()
+	if s.maxSamples > 0 {
+		if s.storedSamples >= s.maxSamples {
+			// We reached the maximum number of samples we can keep in memory, so we randomly
+			// replace a sample.
+			r := r.Int63n(s.storedSamples)
+			if r < s.maxSamples {
+				s.data[r] = v
+			}
+		} else {
+			s.data[s.storedSamples] = v
+			s.storedSamples++
+		}
+	} else {
+		// This code path appends to the slice since we did not pre-allocate memory in this case.
+		s.sampleUnsafe(v)
+	}
+}
+
+func (s *bufferedMetric) skipSample() {
+	atomic.AddInt64(&s.totalSamples, 1)
 }
 
 func (s *bufferedMetric) flushUnsafe() metric {
@@ -142,40 +184,62 @@ func (s *bufferedMetric) flushUnsafe() metric {
 		metricType: s.mtype,
 		name:       s.name,
 		stags:      s.tags,
-		rate:       1,
+		rate:       float64(len(s.data)) / float64(s.totalSamples),
 		fvalues:    s.data,
 	}
 }
 
 type histogramMetric = bufferedMetric
 
-func newHistogramMetric(name string, value float64, stringTags string) *histogramMetric {
+func newHistogramMetric(name string, value float64, stringTags string, maxSamples int64) *histogramMetric {
 	return &histogramMetric{
-		data:  []float64{value},
-		name:  name,
-		tags:  stringTags,
-		mtype: histogramAggregated,
+		data:          newData(value, maxSamples),
+		totalSamples:  1,
+		storedSamples: 1,
+		name:          name,
+		tags:          stringTags,
+		mtype:         histogramAggregated,
+		maxSamples:    maxSamples,
 	}
 }
 
 type distributionMetric = bufferedMetric
 
-func newDistributionMetric(name string, value float64, stringTags string) *distributionMetric {
+func newDistributionMetric(name string, value float64, stringTags string, maxSamples int64) *distributionMetric {
 	return &distributionMetric{
-		data:  []float64{value},
-		name:  name,
-		tags:  stringTags,
-		mtype: distributionAggregated,
+		data:          newData(value, maxSamples),
+		totalSamples:  1,
+		storedSamples: 1,
+		name:          name,
+		tags:          stringTags,
+		mtype:         distributionAggregated,
+		maxSamples:    maxSamples,
 	}
 }
 
 type timingMetric = bufferedMetric
 
-func newTimingMetric(name string, value float64, stringTags string) *timingMetric {
+func newTimingMetric(name string, value float64, stringTags string, maxSamples int64) *timingMetric {
 	return &timingMetric{
-		data:  []float64{value},
-		name:  name,
-		tags:  stringTags,
-		mtype: timingAggregated,
+		data:          newData(value, maxSamples),
+		totalSamples:  1,
+		storedSamples: 1,
+		name:          name,
+		tags:          stringTags,
+		mtype:         timingAggregated,
+		maxSamples:    maxSamples,
+	}
+}
+
+// newData creates a new slice of float64 with the given capacity. If maxSample
+// is less than or equal to 0, it returns a slice with the given value as the
+// only element.
+func newData(value float64, maxSample int64) []float64 {
+	if maxSample <= 0 {
+		return []float64{value}
+	} else {
+		data := make([]float64, maxSample)
+		data[0] = value
+		return data
 	}
 }

statsd/options.go
@@ -8,70 +8,73 @@ import (
 )
 
 var (
-	defaultNamespace                 = ""
-	defaultTags                      = []string{}
-	defaultMaxBytesPerPayload        = 0
-	defaultMaxMessagesPerPayload     = math.MaxInt32
-	defaultBufferPoolSize            = 0
-	defaultBufferFlushInterval       = 100 * time.Millisecond
-	defaultWorkerCount               = 32
-	defaultSenderQueueSize           = 0
-	defaultWriteTimeout              = 100 * time.Millisecond
-	defaultTelemetry                 = true
-	defaultReceivingMode             = mutexMode
-	defaultChannelModeBufferSize     = 4096
-	defaultAggregationFlushInterval  = 2 * time.Second
-	defaultAggregation               = true
-	defaultExtendedAggregation       = false
-	defaultOriginDetection           = true
-	defaultChannelModeErrorsWhenFull = false
-	defaultErrorHandler              = func(error) {}
+	defaultNamespace                    = ""
+	defaultTags                         = []string{}
+	defaultMaxBytesPerPayload           = 0
+	defaultMaxMessagesPerPayload        = math.MaxInt32
+	defaultBufferPoolSize               = 0
+	defaultBufferFlushInterval          = 100 * time.Millisecond
+	defaultWorkerCount                  = 32
+	defaultSenderQueueSize              = 0
+	defaultWriteTimeout                 = 100 * time.Millisecond
+	defaultTelemetry                    = true
+	defaultReceivingMode                = mutexMode
+	defaultChannelModeBufferSize        = 4096
+	defaultAggregationFlushInterval     = 2 * time.Second
+	defaultAggregation                  = true
+	defaultExtendedAggregation          = false
+	defaultMaxBufferedSamplesPerContext = -1
+	defaultOriginDetection              = true
+	defaultChannelModeErrorsWhenFull    = false
+	defaultErrorHandler                 = func(error) {}
 )
 
 // Options contains the configuration options for a client.
 type Options struct {
-	namespace                 string
-	tags                      []string
-	maxBytesPerPayload        int
-	maxMessagesPerPayload     int
-	bufferPoolSize            int
-	bufferFlushInterval       time.Duration
-	workersCount              int
-	senderQueueSize           int
-	writeTimeout              time.Duration
-	telemetry                 bool
-	receiveMode               receivingMode
-	channelModeBufferSize     int
-	aggregationFlushInterval  time.Duration
-	aggregation               bool
-	extendedAggregation       bool
-	telemetryAddr             string
-	originDetection           bool
-	containerID               string
-	channelModeErrorsWhenFull bool
-	errorHandler              ErrorHandler
+	namespace                    string
+	tags                         []string
+	maxBytesPerPayload           int
+	maxMessagesPerPayload        int
+	bufferPoolSize               int
+	bufferFlushInterval          time.Duration
+	workersCount                 int
+	senderQueueSize              int
+	writeTimeout                 time.Duration
+	telemetry                    bool
+	receiveMode                  receivingMode
+	channelModeBufferSize        int
+	aggregationFlushInterval     time.Duration
+	aggregation                  bool
+	extendedAggregation          bool
+	maxBufferedSamplesPerContext int
+	telemetryAddr                string
+	originDetection              bool
+	containerID                  string
+	channelModeErrorsWhenFull    bool
+	errorHandler                 ErrorHandler
 }
 
 func resolveOptions(options []Option) (*Options, error) {
 	o := &Options{
-		namespace:                 defaultNamespace,
-		tags:                      defaultTags,
-		maxBytesPerPayload:        defaultMaxBytesPerPayload,
-		maxMessagesPerPayload:     defaultMaxMessagesPerPayload,
-		bufferPoolSize:            defaultBufferPoolSize,
-		bufferFlushInterval:       defaultBufferFlushInterval,
-		workersCount:              defaultWorkerCount,
-		senderQueueSize:           defaultSenderQueueSize,
-		writeTimeout:              defaultWriteTimeout,
-		telemetry:                 defaultTelemetry,
-		receiveMode:               defaultReceivingMode,
-		channelModeBufferSize:     defaultChannelModeBufferSize,
-		aggregationFlushInterval:  defaultAggregationFlushInterval,
-		aggregation:               defaultAggregation,
-		extendedAggregation:       defaultExtendedAggregation,
-		originDetection:           defaultOriginDetection,
-		channelModeErrorsWhenFull: defaultChannelModeErrorsWhenFull,
-		errorHandler:              defaultErrorHandler,
+		namespace:                    defaultNamespace,
+		tags:                         defaultTags,
+		maxBytesPerPayload:           defaultMaxBytesPerPayload,
+		maxMessagesPerPayload:        defaultMaxMessagesPerPayload,
+		bufferPoolSize:               defaultBufferPoolSize,
+		bufferFlushInterval:          defaultBufferFlushInterval,
+		workersCount:                 defaultWorkerCount,
+		senderQueueSize:              defaultSenderQueueSize,
+		writeTimeout:                 defaultWriteTimeout,
+		telemetry:                    defaultTelemetry,
+		receiveMode:                  defaultReceivingMode,
+		channelModeBufferSize:        defaultChannelModeBufferSize,
+		aggregationFlushInterval:     defaultAggregationFlushInterval,
+		aggregation:                  defaultAggregation,
+		extendedAggregation:          defaultExtendedAggregation,
+		maxBufferedSamplesPerContext: defaultMaxBufferedSamplesPerContext,
+		originDetection:              defaultOriginDetection,
+		channelModeErrorsWhenFull:    defaultChannelModeErrorsWhenFull,
+		errorHandler:                 defaultErrorHandler,
 	}
 
 	for _, option := range options {
@@ -313,6 +316,18 @@ func WithExtendedClientSideAggregation() Option {
 	return func(o *Options) error {
 		o.aggregation = true
 		o.extendedAggregation = true
+		return nil
+	}
+}
+
+// WithMaxSamplesPerContext sets the maximum number of samples that can be aggregated in a single distribution.
+// This feature should be used with `WithExtendedClientSideAggregation`. This limits the number of sample per
+// context for a distribution to a given number.
+// This will enable client side aggregation for all metrics.
+func WithMaxSamplesPerContext(maxSamplesPerDistribution int) Option {
+	return func(o *Options) error {
+		o.aggregation = true
+		o.maxBufferedSamplesPerContext = maxSamplesPerDistribution
 		return nil
 	}
 }

statsd/metrics_test.go
@@ -132,7 +132,7 @@ func TestFlushUnsafeSetMetricSample(t *testing.T) {
 }
 
 func TestNewHistogramMetric(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2")
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -140,7 +140,7 @@ func TestNewHistogramMetric(t *testing.T) {
 }
 
 func TestHistogramMetricSample(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2")
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -149,7 +149,7 @@ func TestHistogramMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeHistogramMetricSample(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2")
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, histogramAggregated)
@@ -170,7 +170,7 @@ func TestFlushUnsafeHistogramMetricSample(t *testing.T) {
 }
 
 func TestNewDistributionMetric(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2")
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -178,7 +178,7 @@ func TestNewDistributionMetric(t *testing.T) {
 }
 
 func TestDistributionMetricSample(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2")
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -187,7 +187,7 @@ func TestDistributionMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeDistributionMetricSample(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2")
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, distributionAggregated)
@@ -208,7 +208,7 @@ func TestFlushUnsafeDistributionMetricSample(t *testing.T) {
 }
 
 func TestNewTimingMetric(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2")
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -216,7 +216,7 @@ func TestNewTimingMetric(t *testing.T) {
 }
 
 func TestTimingMetricSample(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2")
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -225,7 +225,7 @@ func TestTimingMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeTimingMetricSample(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2")
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, timingAggregated)

statsd/aggregator.go
@@ -41,19 +41,24 @@ type aggregator struct {
 	inputMetrics    chan metric
 	stopChannelMode chan struct{}
 	wg              sync.WaitGroup
+
+	// maxSamplesPerContext is the maximum number of samples that can be
+	// aggregated in a single distribution/histogram/set metric (per context).
+	maxSamplesPerContext int64
 }
 
-func newAggregator(c *Client) *aggregator {
+func newAggregator(c *Client, maxSamplesPerContext int64) *aggregator {
 	return &aggregator{
-		client:          c,
-		counts:          countsMap{},
-		gauges:          gaugesMap{},
-		sets:            setsMap{},
-		histograms:      newBufferedContexts(newHistogramMetric),
-		distributions:   newBufferedContexts(newDistributionMetric),
-		timings:         newBufferedContexts(newTimingMetric),
-		closed:          make(chan struct{}),
-		stopChannelMode: make(chan struct{}),
+		client:               c,
+		counts:               countsMap{},
+		gauges:               gaugesMap{},
+		sets:                 setsMap{},
+		histograms:           newBufferedContexts(newHistogramMetric, maxSamplesPerContext),
+		distributions:        newBufferedContexts(newDistributionMetric, maxSamplesPerContext),
+		timings:              newBufferedContexts(newTimingMetric, maxSamplesPerContext),
+		closed:               make(chan struct{}),
+		stopChannelMode:      make(chan struct{}),
+		maxSamplesPerContext: maxSamplesPerContext,
 	}
 }
 

commit c0f68cd3817bfae4cbc53c924e4bce41a14fc2ee

buffered_metrics: reduce a bit lock contention
062e18cef60a416fb19f9203534c936198e79595 introduced
a performance regression by trying to fix the computation
of the actual sampling rate.

This commit takes some shortcuts to reduce lock contention while trying to keep the sampling rate
correct.

statsd/buffered_metric_context.go
@@ -14,7 +14,7 @@ type bufferedMetricContexts struct {
 	nbContext uint64
 	mutex     sync.RWMutex
 	values    bufferedMetricMap
-	newMetric func(string, float64, string) *bufferedMetric
+	newMetric func(string, float64, string, float64) *bufferedMetric
 
 	// Each bufferedMetricContexts uses its own random source and random
 	// lock to prevent goroutines from contending for the lock on the
@@ -25,11 +25,11 @@ type bufferedMetricContexts struct {
 	randomLock sync.Mutex
 }
 
-func newBufferedContexts(newMetric func(string, float64, string, int64) *bufferedMetric, maxSamples int64) bufferedMetricContexts {
+func newBufferedContexts(newMetric func(string, float64, string, int64, float64) *bufferedMetric, maxSamples int64) bufferedMetricContexts {
 	return bufferedMetricContexts{
 		values: bufferedMetricMap{},
-		newMetric: func(name string, value float64, stringTags string) *bufferedMetric {
-			return newMetric(name, value, stringTags, maxSamples)
+		newMetric: func(name string, value float64, stringTags string, rate float64) *bufferedMetric {
+			return newMetric(name, value, stringTags, maxSamples, rate)
 		},
 		// Note that calling "time.Now().UnixNano()" repeatedly quickly may return
 		// very similar values. That's fine for seeding the worker-specific random
@@ -57,6 +57,15 @@ func (bc *bufferedMetricContexts) flush(metrics []metric) []metric {
 func (bc *bufferedMetricContexts) sample(name string, value float64, tags []string, rate float64) error {
 	keepingSample := shouldSample(rate, bc.random, &bc.randomLock)
 
+	// If we don't keep the sample, return early, if we do store *first* observed sampling rate in the metric.
+	// This is the *wrong* behavior but it's the one we had before and the alternative would increase lock contention too
+	// much with the current code.
+	// TODO: change this behavior in the future, probably by introducing thread-local storage and lockless stuctures.
+	// If this code is removed, also remove the observed sampling rate in the metric and fix `bufferedMetric.flushUnsafe()`
+	if !keepingSample {
+		return nil
+	}
+
 	context, stringTags := getContextAndTags(name, tags)
 	var v *bufferedMetric = nil
 
@@ -71,7 +80,7 @@ func (bc *bufferedMetricContexts) sample(name string, value float64, tags []stri
 		v, _ = bc.values[context]
 		if v == nil {
 			// If we might keep a sample that we should have skipped, but that should not drastically affect performances.
-			bc.values[context] = bc.newMetric(name, value, stringTags)
+			bc.values[context] = bc.newMetric(name, value, stringTags, rate)
 			// We added a new value, we need to unlock the mutex and quit
 			bc.mutex.Unlock()
 			return nil

statsd/metrics.go
@@ -140,6 +140,11 @@ type bufferedMetric struct {
 
 	// maxSamples is the maximum number of samples we keep in memory
 	maxSamples int64
+
+	// The first observed user-specified sample rate. This is used only if
+	// we have observed a single sample so far. If we have observed multiple
+	// samples, we use the actual sampling rate.
+	specifiedRate float64
 }
 
 func (s *bufferedMetric) sample(v float64) {
@@ -184,18 +189,30 @@ func (s *bufferedMetric) skipSample() {
 }
 
 func (s *bufferedMetric) flushUnsafe() metric {
+	totalSample := atomic.LoadInt64(&s.totalSamples)
+	var rate float64
+
+	// If the user had a specified rate send it because we don't know better.
+	// This code should be removed once we can also remove the early return at the top of
+	// `bufferedMetricContexts.sample`
+	if s.specifiedRate != 1.0 {
+		rate = s.specifiedRate
+	} else {
+		rate = float64(s.storedSamples) / float64(totalSample)
+	}
+
 	return metric{
 		metricType: s.mtype,
 		name:       s.name,
 		stags:      s.tags,
-		rate:       float64(s.storedSamples) / float64(atomic.LoadInt64(&s.totalSamples)),
+		rate:       rate,
 		fvalues:    s.data[:s.storedSamples],
 	}
 }
 
 type histogramMetric = bufferedMetric
 
-func newHistogramMetric(name string, value float64, stringTags string, maxSamples int64) *histogramMetric {
+func newHistogramMetric(name string, value float64, stringTags string, maxSamples int64, rate float64) *histogramMetric {
 	return &histogramMetric{
 		data:          newData(value, maxSamples),
 		totalSamples:  1,
@@ -204,12 +221,13 @@ func newHistogramMetric(name string, value float64, stringTags string, maxSample
 		tags:          stringTags,
 		mtype:         histogramAggregated,
 		maxSamples:    maxSamples,
+		specifiedRate: rate,
 	}
 }
 
 type distributionMetric = bufferedMetric
 
-func newDistributionMetric(name string, value float64, stringTags string, maxSamples int64) *distributionMetric {
+func newDistributionMetric(name string, value float64, stringTags string, maxSamples int64, rate float64) *distributionMetric {
 	return &distributionMetric{
 		data:          newData(value, maxSamples),
 		totalSamples:  1,
@@ -218,12 +236,13 @@ func newDistributionMetric(name string, value float64, stringTags string, maxSam
 		tags:          stringTags,
 		mtype:         distributionAggregated,
 		maxSamples:    maxSamples,
+		specifiedRate: rate,
 	}
 }
 
 type timingMetric = bufferedMetric
 
-func newTimingMetric(name string, value float64, stringTags string, maxSamples int64) *timingMetric {
+func newTimingMetric(name string, value float64, stringTags string, maxSamples int64, rate float64) *timingMetric {
 	return &timingMetric{
 		data:          newData(value, maxSamples),
 		totalSamples:  1,
@@ -232,6 +251,7 @@ func newTimingMetric(name string, value float64, stringTags string, maxSamples i
 		tags:          stringTags,
 		mtype:         timingAggregated,
 		maxSamples:    maxSamples,
+		specifiedRate: rate,
 	}
 }
 

statsd/options.go
@@ -312,6 +312,8 @@ func WithoutClientSideAggregation() Option {
 
 // WithExtendedClientSideAggregation enables client side aggregation for all types. This feature is only compatible with
 // Agent's version >=6.25.0 && <7.0.0 or Agent's versions >=7.25.0.
+// When enabled the use of `rate` with distribution is discouraged and `WithMaxSamplesPerContext()` should be used.
+// If `rate` is used with different values of `rate` the resulting rate is not guaranteed to be correct.
 func WithExtendedClientSideAggregation() Option {
 	return func(o *Options) error {
 		o.aggregation = true

statsd/metrics_test.go
@@ -132,7 +132,7 @@ func TestFlushUnsafeSetMetricSample(t *testing.T) {
 }
 
 func TestNewHistogramMetric(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -140,7 +140,7 @@ func TestNewHistogramMetric(t *testing.T) {
 }
 
 func TestHistogramMetricSample(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -149,7 +149,7 @@ func TestHistogramMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeHistogramMetricSample(t *testing.T) {
-	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0)
+	s := newHistogramMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, histogramAggregated)
@@ -170,7 +170,7 @@ func TestFlushUnsafeHistogramMetricSample(t *testing.T) {
 }
 
 func TestNewDistributionMetric(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -178,7 +178,7 @@ func TestNewDistributionMetric(t *testing.T) {
 }
 
 func TestDistributionMetricSample(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -187,7 +187,7 @@ func TestDistributionMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeDistributionMetricSample(t *testing.T) {
-	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0)
+	s := newDistributionMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, distributionAggregated)
@@ -208,7 +208,7 @@ func TestFlushUnsafeDistributionMetricSample(t *testing.T) {
 }
 
 func TestNewTimingMetric(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	assert.Equal(t, s.data, []float64{1.0})
 	assert.Equal(t, s.name, "test")
 	assert.Equal(t, s.tags, "tag1,tag2")
@@ -216,7 +216,7 @@ func TestNewTimingMetric(t *testing.T) {
 }
 
 func TestTimingMetricSample(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	s.sample(123.45)
 	assert.Equal(t, s.data, []float64{1.0, 123.45})
 	assert.Equal(t, s.name, "test")
@@ -225,7 +225,7 @@ func TestTimingMetricSample(t *testing.T) {
 }
 
 func TestFlushUnsafeTimingMetricSample(t *testing.T) {
-	s := newTimingMetric("test", 1.0, "tag1,tag2", 0)
+	s := newTimingMetric("test", 1.0, "tag1,tag2", 0, 1.0)
 	m := s.flushUnsafe()
 
 	assert.Equal(t, m.metricType, timingAggregated)