6a9e0e3f8d86d371402c96068057132922a1506d
Carlos Roman <carlos.roman@datadoghq.com>
2026-01-02 17:28:15 +0000

    Running all tests in Go 1.12

--- statsd/worker_test.go
+++ statsd/worker_test.go
@@ -9,7 +9,8 @@ import (
 
 func TestShouldSample(t *testing.T) {
 	rates := []float64{0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99, 1.0}
-	iterations := 50_000
+	// leave as 50000 rather than 50_000 as this breaks in Go 1.12
+	iterations := 50000
 
 	for _, rate := range rates {
 		rate := rate // Capture range variable.
