93a16c484e12a687d1051a08d6b0a1befb18000f
Carlos Roman <carlos.roman@datadoghq.com>
2026-01-30 12:37:44 +0000

    Updated BenchmarkAggregatorSharding with some improvements

--- statsd/aggregator_benchmark_test.go
+++ statsd/aggregator_benchmark_test.go
@@ -5,8 +5,18 @@ import (
 	"testing"
 )
 
+// Prevent compiler from optimizing away function calls
+var benchErr error
+
 func BenchmarkAggregatorSharding(b *testing.B) {
 	shardCounts := []int{1, 2, 3, 4, 5, 6, 8, 16, 32, 64, 128, 256}
+
+	// Pre-generate metric names to avoid measuring fmt.Sprintf performance
+	const numMetrics = 100000
+	metricNames := make([]string, numMetrics)
+	for i := 0; i < numMetrics; i++ {
+		metricNames[i] = fmt.Sprintf("metric.%d", i)
+	}
 
 	for _, shards := range shardCounts {
 		b.Run(fmt.Sprintf("Shards_%d", shards), func(b *testing.B) {
@@ -15,15 +25,20 @@ func BenchmarkAggregatorSharding(b *testing.B) {
 
 			b.ResetTimer()
 			b.RunParallel(func(pb *testing.PB) {
+				var err error
 				i := 0
 				for pb.Next() {
+					name := metricNames[i%numMetrics]
 					i++
-					name := fmt.Sprintf("metric.%d", i%100000)
-					a.count(name, 1, tags, CardinalityLow)
-					a.gauge(name, 10.0, tags, CardinalityLow)
-					a.set(name, "val", tags, CardinalityLow)
+					err = a.count(name, 1, tags, CardinalityLow)
+					err = a.gauge(name, 10.0, tags, CardinalityLow)
+					err = a.set(name, "val", tags, CardinalityLow)
 				}
+				benchErr = err
 			})
 		})
+		if benchErr != nil {
+			b.Fatal(benchErr)
+		}
 	}
 }
